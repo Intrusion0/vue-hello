0d2f05776aed54609d35f770a4e9c55b7ffe7035
Mario <lombardomario01@gmail.com>
2021-11-10 17:02:31 +0100

    methods vue bonus

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -12,6 +12,16 @@ var app = new Vue({
     data: {
         message: 'Benvenuto VUE!!!',
         namePict: 'unsplashTest',
-        image: 'https://unsplash.it/300/300?image='
+        image: 'https://unsplash.it/300/300?image=',
+        testHtml: `<ul><li>Ciao sono un elemento di una lista</li></ul>`
+    },
+    methods: {
+        test: function() {
+            if (this.image == 'https://unsplash.it/300/300?image=') {
+                this.image = 'https://unsplash.it/350/300?image=';
+            } else if (this.image == 'https://unsplash.it/350/300?image=') {
+                this.image = 'https://unsplash.it/300/300?image=';
+            }
+        }
     }
 }) 
