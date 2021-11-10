8537703560ab843e1fffc6d13055a3796821e58f
Mario <lombardomario01@gmail.com>
2021-11-10 15:03:30 +0100

    Bonus completed

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -10,6 +10,8 @@ Aggiungere alla pagina un’immagine, presa anch’essa da un data.
 var app = new Vue({
     el: '#myApp',
     data: {
-        message: 'Benvenuto VUE!!!'
+        message: 'Benvenuto VUE!!!',
+        namePict: 'unsplashTest',
+        image: 'https://unsplash.it/300/300?image='
     }
 }) 
